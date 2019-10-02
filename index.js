a7678469f2c96f1cd4f6da16bfc98e29a8e4799a
Jonathan Martin <me@nybblr.com>
2019-10-02 16:09:44 -0400

    Discover markers in any scope with traversal.

--- index.js
+++ index.js
@@ -5,10 +5,12 @@ import templator from "@babel/template";
 import parser from '@babel/parser';
 import generator from '@babel/generator';
 import types from "@babel/types";
+import traverser from "@babel/traverse";
 
 const {parse} = parser;
 const template = templator.default;
 const generate = generator.default;
+const traverse = traverser.default;
 
 const prefix = template.statements(`
   let __poofSnoop = {};
@@ -68,25 +70,29 @@ let poof = async (input) => {
   let ast = parse(code);
   let {body} = ast.program;
   // console.error(deep(body));
-  body.forEach(s => {
-    let marker = getMarker(s);
-    if (!marker) { return; }
-    let key = types.stringLiteral(
-      keyForMarker(marker)
-    );
-    //console.error(deep(statement));
-    switch (s.type) {
-      case 'ExpressionStatement': {
-        let expression = s.expression;
-        let newExpression = instrument({ key, expression });
-        s.expression = newExpression;
-        break;
-      }
-      case 'VariableDeclaration': {
-        let expression = s.declarations[0].init;
-        let newExpression = instrument({ key, expression });
-        s.declarations[0].init = newExpression;
-        break;
+  traverse(ast, {
+    enter(path) {
+      let s = path.node;
+
+      let marker = getMarker(s);
+      if (!marker) { return; }
+      let key = types.stringLiteral(
+        keyForMarker(marker)
+      );
+      //console.error(deep(statement));
+      switch (s.type) {
+        case 'ExpressionStatement': {
+          let expression = s.expression;
+          let newExpression = instrument({ key, expression });
+          s.expression = newExpression;
+          break;
+        }
+        case 'VariableDeclaration': {
+          let expression = s.declarations[0].init;
+          let newExpression = instrument({ key, expression });
+          s.declarations[0].init = newExpression;
+          break;
+        }
       }
     }
   });
